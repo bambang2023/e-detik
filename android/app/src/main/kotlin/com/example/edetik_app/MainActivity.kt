cb6f8e66d10b10b788ad8df445900bd6077a6d61
Handika Rafli Aryatama <handika.aryatama@gmail.com>
2025-02-13 16:53:17 +0700

    first init

---
+++ android/app/src/main/kotlin/com/example/edetik_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.edetik_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:edetik_app/features/auth/login/page/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: LoginPage(),
+    ),
+  );
+}
